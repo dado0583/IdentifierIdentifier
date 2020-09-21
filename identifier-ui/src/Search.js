3d2155544b555446b394e2f051d493ad65daed4d
Ethan Lipkind <Ethan.lipkind@gmail.com>
2020-09-21 07:20:14 -0400

    token.token -> props.token for readability

--- identifier-ui/src/Search.js
+++ identifier-ui/src/Search.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 
 const API_URL = 'https://mkd29ltjoa.execute-api.us-east-1.amazonaws.com/default/WhichIdentifier'
                  
-const Search = (token) => {
+const Search = (props) => {
   const [query, setQuery] = useState('');
   const submit = () => {
     const payload = [query];
-    const headers = {headers : {Dave: token.token}}
+    const headers = {headers : {Dave: props.token}}
 
     axios.post(API_URL, payload, headers).then((resp) => {
       console.log('got API response', resp)
